a441cbb22e04687fc8b78cdc4cd30107b9178925
NikolaiM <testerus.nikolay@yandex.ru>
2024-05-31 09:17:06 +0300

    Задание 2 - Добавлен класс PointTask() и метод расчета distance(Point point)

--- ls-java-core/src/main/java/PointTask/Point.java
+++ ls-java-core/src/main/java/PointTask/Point.java
@@ -1,8 +1,12 @@
 package PointTask;
 
+import static java.lang.Math.*;
+
 public class Point {
     double pointX;
     double pointY;
+
+    public Point() { }
 
     public Point(double pointX, double pointY) {
         this.pointX = pointX;
@@ -23,5 +27,19 @@ public class Point {
 
     public void setPointY(double pointY) {
         this.pointY = pointY;
+    }
+
+    public double distance(Point point){
+        double distanceAB;
+        double pointAx = this.pointX;
+        double pointAy = this.pointY;
+        double pointBx = point.pointX;
+        double pointBy = point.pointY;
+
+
+        //Рассчет расстояниия между двумя точками A(x,y) и B(x,y)
+        distanceAB = sqrt(pow((pointBx - pointAx),2) + pow((pointAy - pointBy),2));
+
+        return distanceAB;
     }
 }
---
+++ ls-java-core/src/main/java/PointTask/PointTask.java
@@ -0,0 +1,11 @@
+package PointTask;
+
+public class PointTask {
+    public static void main(String[] args) {
+
+        Point pointA = new Point();
+        Point pointB = new Point();
+
+        System.out.println("Hello world!");
+    }
+}
