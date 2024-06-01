a68e43669206f0fa6e4a3c7b81c9baa8d47b15e6
NikolaiM <testerus.nikolay@yandex.ru>
2024-06-01 22:57:43 +0300

    Задание 2 - Добавление логики и правки класса PointTask.

--- ls-java-core/src/main/java/PointTask/Point.java
+++ ls-java-core/src/main/java/PointTask/Point.java
@@ -1,17 +1,11 @@
 package PointTask;
 
+import java.util.Scanner;
 import static java.lang.Math.*;
 
 public class Point {
     double pointX;
     double pointY;
-
-    public Point() { }
-
-    public Point(double pointX, double pointY) {
-        this.pointX = pointX;
-        this.pointY = pointY;
-    }
 
     public double getPointX() {
         return pointX;
@@ -29,17 +23,45 @@ public class Point {
         this.pointY = pointY;
     }
 
+    /***
+     * Расчет расстояния между двумя точками
+     * @param point - в качестве параметра передается переменная класса Point
+     * @return расстояние между 2 точками
+     */
     public double distance(Point point){
-        double distanceAB;
+
         double pointAx = this.pointX;
         double pointAy = this.pointY;
-        double pointBx = point.pointX;
-        double pointBy = point.pointY;
-
+        double pointBx = point.getPointX();
+        double pointBy = point.getPointY();
 
         //Рассчет расстояниия между двумя точками A(x,y) и B(x,y)
-        distanceAB = sqrt(pow((pointBx - pointAx),2) + pow((pointAy - pointBy),2));
+        return sqrt(pow((pointBx - pointAx),2) + pow((pointBy - pointAy),2));
+    }
 
-        return distanceAB;
+    public void MethodOfEnteringCoordinatesPoint() {
+
+        double coordinateX, coordinateY;
+        Scanner sc = new Scanner(System.in);
+        try {
+              System.out.print("Координата X = ");
+              coordinateX = sc.nextDouble();
+              this.setPointX(coordinateX);
+
+              System.out.print("Координата Y = ");
+              coordinateY = sc.nextDouble();
+              this.setPointY(coordinateY);
+
+        } catch (Exception e) {
+              System.out.println("Сообщение об ошибке. " +
+                      "\nВведено не корретное значение." +
+                      "\nДопусткается ввод с разделителем \",\" (5,8870).");
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Координаты точки: Point("+ this.pointX + ";" + this.pointY + ")";
     }
 }
--- ls-java-core/src/main/java/PointTask/PointTask.java
+++ ls-java-core/src/main/java/PointTask/PointTask.java
@@ -6,6 +6,17 @@ public class PointTask {
         Point pointA = new Point();
         Point pointB = new Point();
 
-        System.out.println("Hello world!");
+        //Заполнение координат точки А(x,y)
+        System.out.println("Введите значений точки А(x,y).\nДопусткается ввод с разделителем \",\" (5,8870).");
+        pointA.MethodOfEnteringCoordinatesPoint();
+        System.out.println(pointA.toString());
+
+        //Заполнение координат точки B(x,y)
+        System.out.println("Введите значений точки B(x,y).\nДопусткается ввод с разделителем \",\" (5,8870). ");
+        pointB.MethodOfEnteringCoordinatesPoint();
+        System.out.println(pointB.toString());
+
+        System.out.println("Вывод расстояния между точками A и B: " +
+              pointA.distance(pointB));
     }
 }
