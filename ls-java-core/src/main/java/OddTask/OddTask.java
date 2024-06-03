9050c0459ba3042a97da142ddc10af1528e13bd8
NikolaiM <testerus.nikolay@yandex.ru>
2024-06-03 22:31:19 +0300

    Задание 2 - Исправлен класс OddTask добавлен цикл ввода значений.

--- ls-java-core/src/main/java/OddTask/OddTask.java
+++ ls-java-core/src/main/java/OddTask/OddTask.java
@@ -8,25 +8,36 @@ public class OddTask {
 
         int number;
         Scanner sc = new Scanner(System.in);
-        try {
-            System.out.print("Введите целое число: ");
-            number = sc.nextInt();
+        do {
+            try {
+                System.out.print("\nВведите целое число или для выхода введите 0.\n");
+                System.out.print("Введите число: ");
+                number = sc.nextInt();
 
-            //Проверка результата
-            if(isOdd(number))
-                System.out.println("Вы ввели ЧЕТНОЕ число.");
-            else
-                System.out.println("Вы ввели НЕЧЕТНОЕ число.");
+                //Проверка результата
+                if(number != 0)
+                {
+                    if (isOdd(number))
+                        System.out.println("Вы ввели ЧЕТНОЕ число.\n");
+                    else
+                        System.out.println("Вы ввели НЕЧЕТНОЕ число.\n");
+                }
+                else
+                    break;
 
-
-        } catch (Exception e) {
-            System.out.println("Сообщение об ошибке. " +
-                    "\nВведено не корретное значение." +
-                    "\nДопусткается ввод целых чисел.");
-        }
-
+            } catch (Exception e) {
+                System.out.println("Сообщение об ошибке. " +
+                        "\nВведено не корретное значение." +
+                        "\nДопусткается ввод целых чисел.");
+            }
+        }while (true);
     }
 
+    /**
+     * Функция проверки числа четное/нечетное
+     * @param number проверяемое целое число.
+     * @return - Результат true четное иначе false
+     */
     public  static boolean  isOdd(int number){
 
         boolean flag = false;
