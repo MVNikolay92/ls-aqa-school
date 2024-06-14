be42a1c14deda3492e0eb55cfd4e9f34a3058236
NikolaiM <testerus.nikolay@yandex.ru>
2024-06-14 22:54:18 +0300

    Задание 3 - Добавлен тестовый метод pointToStringPositiveTest для проверки вывода строки класса Point

--- ls-java-core/src/test/java/PointTaskTest/PointTaskTest.java
+++ ls-java-core/src/test/java/PointTaskTest/PointTaskTest.java
@@ -7,12 +7,19 @@ public class PointTaskTest {
 
     Point pointA = new Point(42.000,64.0000);
     Point pointB = new Point(9.9284,61.9442);
-    double result = 32.13742118154474;
+    double CONSTANT_RESULT = 32.13742118154474;
+    String CONSTANT_POINT_A = "Координаты точки: Point(42.0;64.0)";
 
 
     @Test
     @DisplayName("Позитивная проверка расчета растояния м/у точками")
     public void distancePositiveTest(){
-        Assertions.assertEquals(result,pointA.distance(pointB));
+        Assertions.assertEquals(CONSTANT_RESULT,pointA.distance(pointB));
+    }
+
+    @Test
+    @DisplayName("Позитивная проверка вывода координат точки")
+    public void pointToStringPositiveTest(){
+        Assertions.assertEquals(CONSTANT_POINT_A, pointA.toString());
     }
 }
