e0c093bf0945dc9de5b81d74b67a9b49b3de683d
NikolaiM <testerus.nikolay@yandex.ru>
2024-06-17 23:40:16 +0300

    Задание 3 - Константы вынесены в отдельный класс.

--- ls-java-core/src/test/java/PointTaskTest/PointTaskTest.java
+++ ls-java-core/src/test/java/PointTaskTest/PointTaskTest.java
@@ -2,14 +2,12 @@ package PointTaskTest;
 
 import ru.ls.qa.school.core.PointTask.Point;
 import org.junit.jupiter.api.*;
+import static PointTaskTest.PointConstants.*;
 
 public class PointTaskTest {
 
     Point pointA = new Point(42.000,64.0000);
     Point pointB = new Point(9.9284,61.9442);
-    double CONSTANT_RESULT = 32.13742118154474;
-    String CONSTANT_POINT_A = "Координаты точки: Point(42.0;64.0)";
-
 
     @Test
     @DisplayName("Позитивная проверка расчета растояния м/у точками")
